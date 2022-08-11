94cc3f07dfb3ddf0c68bd2b43fcaa932df0536f9
Griffin Roupe <groupe@renci.org>
2022-08-11 13:16:26 -0400

    Unique record identifiers

--- relationships/udfs.py
+++ relationships/udfs.py
@@ -1,10 +1,20 @@
-from random import randint
+from random import randint, choice
 from .register import udf
 
+"""
+UDFs are currently ran once during generation, and that is the only time.
+Therefore, this rudimentary solution be alright to use. Not great, but it gets the job done rather than
+adding further complexity to configuration.
+"""
+used_record_ids = []
 @udf("test_record_id")
 def test_record_id():
     """ Return a string in the format "TEST_######" where "######" represents a random 6 digit, 0-padded number. """
-    x = str(randint(0, 999999)).rjust(6, "0")
+    # Range upper argument is exclusive, so choices are actually from 0-999999
+    num_choices = list(set(range(0, 1000000)).difference(used_record_ids))
+    if len(num_choices) == 0:
+        raise Exception("Ran out of record ids. You are may be trying to generate more records than the amount of possible record_ids.")
+    x = str(choice(num_choices)).rjust(6, "0")
     return f"TEST_{x}"
 
 @udf("age_generator")
